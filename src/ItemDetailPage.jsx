a928558a62b467ea0e4932f9e46deeaa12d9ceb8
javi <javitxin1977@gmail.com>
2024-04-17 11:36:11 +0200

    ending up

--- src/ItemDetailPage.jsx
+++ src/ItemDetailPage.jsx
@@ -1,7 +1,12 @@
-const ItemDetailPage = () => {
+
+
+
+const ItemDetailPage = ({item}) => {
 
   return (
     <>
+      <h3>{item.title}</h3>
+      <p>Completed: {`${item.Completed}`}</p>
     </>
  
   );
